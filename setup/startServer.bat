de2d6410bd5da4907faae3542978b928fedca04d
Sumit Dhyani <sumit.dhyani@iongroup.com>
2022-08-19 12:45:27 +0530

    Changed the name of the image from market_gateway to python_base

--- setup/startServer.bat
+++ setup/startServer.bat
@@ -1,8 +1,7 @@
 docker rm setup_mkt_gateway_1
 docker rm setup_kafka_1
 docker rm setup_zookeeper_1
-docker rmi market_gateway
 del /F /Q docker.sock
 del /F /Q kafka
-docker build . -t market_gateway
-docker-compose up
+docker build . -t python_base
+docker-compose up -d
