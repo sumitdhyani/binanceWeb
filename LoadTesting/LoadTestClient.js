fc9b390e2f94d0d13f932c84a25b664d67acfafb
sumitdhyani <sumitdhyani1@gmail.com>
2023-02-19 11:49:08 +0530

    Added the load testing client, 1st version

---
+++ LoadTesting/LoadTestClient.js
@@ -0,0 +1,100 @@
+const { launch, subUnsub } = require('../ClientLayerLibrary/ClientInterface')
+const prompt = require("prompt-async");
+const fs = require('fs');
+const readline = require('readline');
+const CommonUtils = require("../CommonUtils")
+const symbolDict = new Map()
+
+function sleep(ms) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        //console.log("Done waiting");
+        resolve(ms)
+      }, ms )
+    })
+}  
+
+async function loadSymbols() {
+    const fileStream = fs.createReadStream('symbols.txt');
+
+    const rl = readline.createInterface({
+        input: fileStream,
+        crlfDelay: Infinity
+    });
+
+    for await (const line of rl) {
+        dict = JSON.parse(line)
+        const desc = dict["baseAsset"] + " vs " + dict["quoteAsset"]
+        dict["description"] = desc
+        symbol = dict["symbol"]
+        symbolDict.set(symbol, dict)
+    }
+}
+
+function onData(data){
+    console.log(`Received data: ${JSON.stringify(data)}`)
+}
+
+launch({auth_server : "http://127.0.0.1:90", credentials : {user : "test_user", password : "test_pwd"}}, onData, (msg)=>{/*console.log(msg)*/})
+
+function actionForVirtualSymbol(action, asset, currency, bridge)
+{
+    subUnsub({action : action,
+              asset : asset,
+              currency : currency, 
+              bridge : bridge})
+}
+
+
+async function actionForNormalSymbol(action, symbol)
+{
+    subUnsub({action : action,
+              symbol : symbol,
+              exchange : "BINANCE"})
+}
+
+//This is the entry point of the application, this method is passed to the start method as you will see below
+async function mainLoop()
+{
+    low = parseInt(process.argv[2])
+    mid = parseInt(process.argv[3])
+    high = parseInt(process.argv[4])
+    delay = parseInt(process.argv[5])
+    const localSymbols = []
+    await loadSymbols()
+    let i = 0
+    for(const [symbol, obj] of symbolDict){
+        localSymbols.push(symbol)
+        if(++i == high){
+            break
+        }
+    }
+
+    symbolDict.clear()
+
+    while(true){
+        for(i = 0; i < high; i++){
+            //console.log(localSymbols[i])
+            await sleep(delay)
+            actionForNormalSymbol("subscribe", localSymbols[i])
+        }
+
+        //console.log("###############################################")
+
+        for(i = high - 1; i >= mid; i--){
+            //console.log(localSymbols[i])
+            await sleep(delay)
+            actionForNormalSymbol("unsubscribe", localSymbols[i])
+        }
+
+        //console.log("###############################################")
+
+        for(i = mid; i < high; i++){
+            //console.log(localSymbols[i])
+            await sleep(delay)
+            actionForNormalSymbol("subscribe", localSymbols[i])
+        }
+    }
+}
+
+mainLoop().then(()=>{})
