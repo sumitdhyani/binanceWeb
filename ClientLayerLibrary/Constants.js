a83860ec5cc1fbea7d9c3ec5fe32de06bc0fef25
sumitdhyani <sumitdhyani1@gmail.com>
2023-05-22 10:09:02 +0530

    Used the modern syntax in the Constants.js file

--- ClientLayerLibrary/Constants.js
+++ ClientLayerLibrary/Constants.js
@@ -1,10 +1,7 @@
-const constants = {
+export const constants = {
     tags : {exchange : "exchange", action : "action"},
     exchanges: {exch_binance : "BINANCE"},
     instrumets : {currency_pair : "currency_pair",
                   bridged_currency_pair : "bridged_currency_pair"},
     fsm_events : { client_intent : "client_intent"}
 }
-
-
-module.exports.constants = constants
